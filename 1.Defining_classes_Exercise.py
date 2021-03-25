556548d85940173a0480dff259a2cdc24c945e2e
Polina Mincheva <81371141+polly-rm@users.noreply.github.com>
2021-03-25 19:18:05 +0200

    Update 1.Defining_classes_Exercise.py

--- 1.Defining_classes_Exercise.py
+++ 1.Defining_classes_Exercise.py
@@ -1,91 +1,91 @@
 # 1.Car
-# class Car:
-#     def __init__(self, name, model, engine):
-#         self.name = name
-#         self.model = model
-#         self.engine = engine
-#
-#     def get_info(self):
-#         return f"This is {self.name} {self.model} with engine {self.engine}"
+class Car:
+    def __init__(self, name, model, engine):
+        self.name = name
+        self.model = model
+        self.engine = engine
+
+    def get_info(self):
+        return f"This is {self.name} {self.model} with engine {self.engine}"
 
 
 # 2.Shop
-# class Shop:
-#     def __init__(self, name, items):
-#         self.name = name
-#         self.items = items
-#
-#     def get_items_count(self):
-#         return len(self.items)
+class Shop:
+    def __init__(self, name, items):
+        self.name = name
+        self.items = items
+
+    def get_items_count(self):
+        return len(self.items)
 
 
 # 3.Hero
-# class Hero:
-#     def __init__(self, name, health):
-#         self.name = name
-#         self.health = health
-#
-#     def defend(self, damage):
-#         self.health -= damage
-#         if self.health <= 0:
-#             self.health = 0
-#             return f"{self.name} was defeated"
-#
-#     def heal(self, amount):
-#         self.health += amount
+class Hero:
+    def __init__(self, name, health):
+        self.name = name
+        self.health = health
+
+    def defend(self, damage):
+        self.health -= damage
+        if self.health <= 0:
+            self.health = 0
+            return f"{self.name} was defeated"
+
+    def heal(self, amount):
+        self.health += amount
 
 
 # 4.Steam_User
-# class SteamUser:
-#     def __init__(self, username, games, played_hours=0):
-#         self.username = username
-#         self.games = games
-#         self.played_hours = played_hours
-#
-#     def play(self, game, hours):
-#         if game in self.games:
-#             self.played_hours += hours
-#             return f"{self.username} is playing {game}"
-#         else:
-#             return f"{game} is not in library"
-#
-#     def buy_game(self, game):
-#         if game not in self.games:
-#             self.games.append(game)
-#             return f"{self.username} bought {game}"
-#         else:
-#             return f"{game} is already in your library"
-#
-#     def stats(self):
-#         return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
+class SteamUser:
+    def __init__(self, username, games, played_hours=0):
+        self.username = username
+        self.games = games
+        self.played_hours = played_hours
+
+    def play(self, game, hours):
+        if game in self.games:
+            self.played_hours += hours
+            return f"{self.username} is playing {game}"
+        else:
+            return f"{game} is not in library"
+
+    def buy_game(self, game):
+        if game not in self.games:
+            self.games.append(game)
+            return f"{self.username} bought {game}"
+        else:
+            return f"{game} is already in your library"
+
+    def stats(self):
+        return f"{self.username} has {len(self.games)} games. Total play time: {self.played_hours}"
 
 
 
 # 5.Programmer
-# class Programmer:
-#     def __init__(self, name, language, skills):
-#         self.name = name
-#         self.language = language
-#         self.skills = skills
-#
-#     def watch_course(self, course_name, language, skills_earned):
-#         if language == self.language:
-#             self.skills += skills_earned
-#             return f"{self.name} watched {course_name}"
-#         else:
-#             return f"{self.name} does not know {language}"
-#
-#     def change_language(self, new_language, skills_needed):
-#         if self.skills >= skills_needed and new_language != self.language:
-#             old_language = self.language
-#             self.language = new_language
-#             return f"{self.name} switched from {old_language} to {new_language}"
-#
-#         elif self.skills >= skills_needed and new_language == self.language:
-#             return f"{self.name} already knows {self.language}"
-#
-#         else:
-#             return f"{self.name} needs {skills_needed-self.skills} more skills"
+class Programmer:
+    def __init__(self, name, language, skills):
+        self.name = name
+        self.language = language
+        self.skills = skills
+
+    def watch_course(self, course_name, language, skills_earned):
+        if language == self.language:
+            self.skills += skills_earned
+            return f"{self.name} watched {course_name}"
+        else:
+            return f"{self.name} does not know {language}"
+
+    def change_language(self, new_language, skills_needed):
+        if self.skills >= skills_needed and new_language != self.language:
+            old_language = self.language
+            self.language = new_language
+            return f"{self.name} switched from {old_language} to {new_language}"
+
+        elif self.skills >= skills_needed and new_language == self.language:
+            return f"{self.name} already knows {self.language}"
+
+        else:
+            return f"{self.name} needs {skills_needed-self.skills} more skills"
 
 
 
